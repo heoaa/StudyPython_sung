ce8371f5dbd3245823beb71728e69e402597ce94
heoaa <0107heo@naver.com>
2022-05-02 17:46:07 +0900

    220502

---
+++ basic/220502/star_print.py
@@ -0,0 +1,23 @@
+# * 찍기
+print('\n--------------------------------------')
+
+for x in range(1, 6) :
+    for y in range(x, 6) :
+        print('*', end='')
+    print('')
+
+
+# end
+print('\n--------------------------------------')
+
+print('Hello', end=' ')
+print('World')
+
+
+# # 구구단
+# print('\n--------------------------------------')
+
+# for x in int(range(1, 10)) :
+#     for y in int(range(1, 10)) :
+#         print(f'{x} * {y} = {x*y}', end='')
+#     print(' ')
